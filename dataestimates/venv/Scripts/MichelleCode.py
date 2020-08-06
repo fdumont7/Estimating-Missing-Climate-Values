abcd63b88e065d241839bfb8a02d9919ade8a675
Dumont <dumont.faryn@epa.gov>
2020-08-06 10:48:12 -0400

    added MichelleCode and v1

---
+++ dataestimates/venv/Scripts/MichelleCode.py
@@ -0,0 +1,26 @@
+#  Normal Ratio
+#  Aramanuous 2020 equation 3
+
+import math
+
+N1 = 43.33
+N2 = 45.55
+N3 = 42.42
+N4 = 41.22
+N5 = 40.37
+
+x1 = 4.2
+x2 = 3.8
+x3 = 3.9
+x4 = 4.0
+
+term1 = N5/N1 * x1
+term2 = N5/N2 * x2
+term3 = N5/N3 * x3
+term4 = N5/N4 * x4
+
+n = 5.0
+
+X5 = 1/n *(term1 + term2 + term3 + term4)
+
+print(X5)
---
+++ dataestimates/venv/Scripts/v1.py
@@ -0,0 +1,20 @@
+#  Normal Ratio
+#  Aramanuous 2020 equation 3
+
+import math
+N = [43.33,45.55,42.42,41.22,40.37]
+x = [4.2,3.8, 3.9, 4.0]
+term = []
+num = 0
+for i in x:
+    term.append(N[-1]/N[num] * i)
+    num=num+1
+
+n = 5.0
+termSum=0
+for i in term:
+    termSum= termSum + i
+
+X5 = 1/n * termSum
+
+print(X5)
