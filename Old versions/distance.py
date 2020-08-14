8054d9d208ee4714e9366b6c559d5debe3016b3f
michelleannesimon <simon.michelle@epa.gov>
2020-08-14 10:17:43 -0400

    This is a python program to calculate distance

---
+++ Old versions/distance.py
@@ -0,0 +1,35 @@
+
+# From https://www.geeksforgeeks.org/program-distance-two-points-earth/#:~:text=For%20this%20divide%20the%20values,is%20the%20radius%20of%20Earth.
+# Python 3 program to calculate Distance Between Two Points on Earth
+from math import radians, cos, sin, asin, sqrt
+
+
+def distance(lat1, lat2, lon1, lon2):
+    # The math module contains a function named
+    # radians which converts from degrees to radians.
+    lon1 = radians(lon1)
+    lon2 = radians(lon2)
+    lat1 = radians(lat1)
+    lat2 = radians(lat2)
+
+    # Haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+
+    c = 2 * asin(sqrt(a))
+
+    # Radius of earth in kilometers. Use 3956 for miles
+    r = 6371
+
+    # calculate the result
+    return (c * r)
+
+
+# driver code
+lat1 = 39.5167
+lat2 = 39.5167
+lon1 = -84.4167
+lon2 = -84.4167
+print(distance(lat1, lat2, lon1, lon2), "K.M")
+
