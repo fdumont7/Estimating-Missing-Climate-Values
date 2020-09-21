2ea925d5f9208dc75923a3ef38899de41a417725
chocobochic7 <chocobochic7@gmail.com>
2020-09-21 15:45:59 -0400

    began work on adding in reading of .dat/txt files

--- FileInformation.py
+++ FileInformation.py
@@ -4,72 +4,38 @@ import xlrd #whats used to read excel document. get by 'pip install xlrd'
 class FileInformation:
 
 	def __init__(self):
+		self.data = getData(input("What file would you like to open (please put in full location)\n"))
+		while self.data == 0:
+			print("invalid file name please try again")
+			self.data = getData(input("What file would you like to open (please put in full location)\n"))
 		
-		while True:
-			try:
-				myFile = input("What file would you like to open (please put in full location)\n")
-				workBook = xlrd.open_workbook(myFile)
-				
-				break
-			except OSError:
-				print("invalid file name please try again")
-
-		self.workBook = xlrd.open_workbook(myFile)
-		self.sheet = workBook.sheet_by_index(0)
 		
 
-	#Xi = measured value of the climatic parameter in the surrounding stations
-	#returns a list
-	def getXi(self):
-		
-		x = self.sheet.row_values(1)[1:]
-		x.remove('')
-		return x
+	
+	def getData(self, file):
+		if ".xlsx" in file:
+			workBook = xlrd.open_workbook(file, on_demand = True)
+			sheet = workBook.sheet_by_index(0)
+			data = []
+			#station ID
+			data.append(sheet.col_values(0))
+			#Year
+			data.append(sheet.col_values(2))
+			#month
+			data.append(sheet.col_values(3))
+			#Day
+			data.append(sheet.col_values(4))
+			#Hour
+			data.append(sheet.col_values(5))
+			#Precipitation data
+			data.append(sheet.col_values(6))
+			return data
+		else if ".dat" in file:
 
-	#n = number of surrounding stations.
-	def getn(self):
-		
-		return self.sheet.nrows
-
-	#mean of the available rainfall
-	#returns a list
-	def getNs(self):
-		
-		return float(self.getNi()[-1])
-		
-
-	#mean of the available rainfall data at the ith surrounding stations
-	#returns a float
-	def getNi(self):
-		N = self.sheet.row_values(0)[1:]
-		return N
+		else:
+			return 0
 		
 
-
-	#longitude of the ith nearby station
-	def getLongitude(self):
-		
-		longitude = self.sheet.row_values(2)[1:]
-		return longitude
-
-	#latitude of the ith nearby station
-	def getLatitude(self):
-		
-		latitude = self.sheet.row_values(3)[1:]
-		return latitude
-
-	#distance from the target station to the ith surrounding station
-	def getdi(self):
-		
-		di = self.sheet.row_values(4)[1:]
-		return di
-
-	#distance of friction varying from 1 to 6
-	#returns int k
-	#may need a set k function as well idk where k is held
-	def getk(self):
-		
-		k = self.sheet.row_values(3)[1]
-		return k
+	
 
 		
