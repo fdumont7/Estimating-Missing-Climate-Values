77771d151cf895b60bccbf5b87481dd0e68a38a1
chocobochic7 <chocobochic7@gmail.com>
2020-10-19 13:02:30 -0400

    added more methods as well as more variables

--- run.py
+++ run.py
@@ -9,7 +9,23 @@ FI = FileInformation()
 
 E = Estimate()
 
-print("AA =", E.AA(FI.getXi(), FI.getn()), "\nNormal Ratio =", E.NormalRatio(FI.getNs(), FI.getNi(), FI.getn(), FI.getXi()), "\nGC = ", E.GC(FI.getLongitude(), FI.getLatitude(), FI.getn(), FI.getXi()))
+AA = E.AA(FI.getXi(), FI.getn())
+
+NR = E.NormalRatio(FI.getNs(), FI.getn(), FI.getNi(), FI.getXi())
+
+GC = E.GC(FI.getLongitude(), FI.getLatitude(),  FI.getXi())
+
+NRGC = E.NRGC(FI.getNs(), FI.getNi(), FI.getXi(), FI.getLongitude(), FI.getLatitude())
+
+IDW = E.IDW(FI.getdi(), FI.getk(), FI.getXi())
+
+MIDW = E.MIDW(FI.getdi(), FI.gethi(), FI.getk(), FI.getXi())
+
+CCW = E.CCW(FI.getri(), FI.getXi())
+
+ONRID = E.ONRID(FI.getNs(), FI.getNi(), FI.getdi(), FI.getXi())
+
+print("AA =", AA , "\nNormal Ratio =", NR , "\nGC = ", GC, "\nNRGC =", NRGC, "\nIDW =", IDW , "\nMIDW =", MIDW, "\nCCW =", CCW , "\nONRID =", ONRID)
 
 
 
