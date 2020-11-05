0d290fc1ff981e22399d699fa9e77b0ef21a3b0a
chocobochic7 <chocobochic7@gmail.com>
2020-11-05 12:06:06 -0500

    Now have the option to write to a .csv file

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 #import v2ReadingExcelDoc
 from FileInformation import FileInformation
 from Estimate import Estimate
+import csv
 
 
 FI = FileInformation()
@@ -25,6 +26,28 @@ ONRID = E.ONRID(FI.getNs(), FI.getNi(), FI.getdi(), FI.getXi())
 
 print("AA =", AA , "\nNR =", NR , "\nGC = ", GC, "\nNRGC =", NRGC, "\nIDW =", IDW , "\nMIDW =", MIDW, "\nCCW=", CCW , "\nONRID =", ONRID)
 
+writetofile = input("\nWould you like to print to a file? (Y/N)")
+
+if writetofile == "Y":
+	fields = ["Equation used", "Result"]
+	rows = [["Simple Arithmetic Average", AA],
+		   ["Normal Ratio", NR],
+		   ["Geographical Coordinates", GC],
+		   ["Normal Ratio with Geographical Coordinates", NRGC],
+		   ["Inverse distance Weighting", IDW],
+		   ["Modified Inverse Distance Weighting", MIDW],
+		   ["Correlation Coefficient Weighted", CCW],
+		   ["Modified Old Normal Ratio with Inverse Distance", ONRID]]
+	fileName = "Estimated-Values.csv"
+	with open(fileName, 'w') as csvfile:  
+		# creating a csv writer object  
+		csvwriter = csv.writer(csvfile)  
+		    
+		# writing the fields  
+		csvwriter.writerow(fields)  
+		    
+		# writing the data rows  
+		csvwriter.writerows(rows) 
 
 
 	
